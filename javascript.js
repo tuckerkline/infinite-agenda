ce83e561815fc62bd6a02453dfb42404aba887a5
Tucker Kline <tucker.kline@gmail.com>
2015-09-15 14:40:15 -0600

    basic functionality

--- javascript.js
+++ javascript.js
@@ -1,10 +1,19 @@
 angular.module('myApp', [])
 
 var mainControllerFunc = function($scope) {
-	
+	$scope.days = []
 
+	for (var i =0 ; i < 10; i++){
+		var currentDate = new Date();
+    	currentDate.setDate(currentDate.getDate()+ i);
+    	currentDate.appointments = []
+    	$scope.days.push(currentDate)
+	}
 
-
+	$scope.addAppointment = function(index) {
+		$scope.addedApt = prompt('Add your appointment here')
+		$scope.days[index].appointments.push($scope.addedApt)
+	}	
 }
 
 angular.module('myApp').controller('mainController', ['$scope', mainControllerFunc])
