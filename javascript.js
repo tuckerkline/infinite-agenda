e03bb162d58ceb44115b8a7cb3c2f17813fbcfbf
Tucker Kline <tucker.kline@gmail.com>
2015-09-16 09:23:28 -0600

    inifnite scrolling kind of works

--- javascript.js
+++ javascript.js
@@ -1,22 +1,28 @@
-angular.module('myApp', [])
+angular.module('myApp', ['infinite-scroll'])
 
 var mainControllerFunc = function($scope) {
 	$scope.days = []
 
-	for (var i =0 ; i < 25; i++){
-		var currentDate = new Date();
-    	currentDate.setDate(currentDate.getDate()+ i);
-    	currentDate.appointments = []
-    	$scope.days.push(currentDate)
+	$scope.getDays = function() {
+		for (var i =0 ; i < 10; i++){
+			var currentDate = new Date();
+	    	currentDate.setDate(currentDate.getDate()+ i);
+	    	currentDate.appointments = []
+	    	$scope.days.push(currentDate)
+		}
 	}
+
+	$scope.getDays();
 
 	$scope.addAppointment = function(index) {
 		$scope.addedApt = prompt('Add your appointment name here')
 		$scope.days[index].appointments.push($scope.addedApt)
 	}
-	$scope.toShow = function(index, day) {
+	$scope.delete = function(index, day) {
 		day.appointments.splice(index, 1)
-		console.log('hi')
+	}
+	$scope.toEdit = function(index) {
+		
 	}
 	
 
