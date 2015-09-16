48d4b2d676d67bf745cba4300d88eb03ee110925
Tucker Kline <tucker.kline@gmail.com>
2015-09-16 10:10:06 -0600

    inifinite scroll works

--- javascript.js
+++ javascript.js
@@ -4,15 +4,31 @@ var mainControllerFunc = function($scope) {
 	$scope.days = []
 
 	$scope.getDays = function() {
-		for (var i =0 ; i < 10; i++){
+		for (var i = 0 ; i < 10; i++){
 			var currentDate = new Date();
 	    	currentDate.setDate(currentDate.getDate()+ i);
 	    	currentDate.appointments = []
 	    	$scope.days.push(currentDate)
 		}
 	}
-
 	$scope.getDays();
+	
+	$scope.a = 10
+	$scope.b = 15
+
+	$scope.getMoreDays = function(a, b) {
+		
+		for (var i = a ; i < b; i++){
+			var currentDate = new Date();
+	    	currentDate.setDate(currentDate.getDate()+ i);
+	    	currentDate.appointments = []
+	    	$scope.days.push(currentDate)
+		    $scope.a++
+		    $scope.b++	 	
+		}
+	}
+
+
 
 	$scope.addAppointment = function(index) {
 		$scope.addedApt = prompt('Add your appointment name here')
